7cb180f17101fc257e4eab11b9c74c287a920d70
paparoneelizabeth@gmail.com <Elizabethpaparone@MacBook-Pro.local>
2019-10-05 21:52:58 -0400

    updated features

--- index.js
+++ index.js
@@ -4,19 +4,41 @@ function getDogImages() {
   // this function calls on the API
   fetch('https://dog.ceo/api/breeds/image/random/3')
     .then(response => response.json())
-    .then(responseJson => displayImages(responseJson))
+    .then(responseImages => displayImages(responseImages))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-function displayImages(responseJson) {
+function displayImages(responseImages) {
   // this function displays the data received from the call
-    console.log(responseJson);
-    $('.js-results-img').replaceWith(
-      `<img src='${responseJson.message}' class='js-results-img'>`)
+    // logs results to the console
+    console.log(responseImages);
+
+    let dogImages = responseImages.body;
+
+    // adds the image to the html
+    $('.js-results-img').html(
+      `<img src='${dogImages.message}' class='js-results-img'>`)
+    
+    // displays the html block
     $('#dog-images').toggleClass('hide');
 }
 
-function handleImages() {
+function watchForm(dogImages) {
+  $('form').submit(function() {
+    event.preventDefault();
+    //let repeat = $('.number-input').val();
+    //let message = responseImages;
+    //let images = responseImages.body;
+    let i;
+    for (i = 0; i <= dogImages.length; i++) {
+      displayImages();
+    };
+  });
+}
+
+// I need to look at the array and then find the given number - 1 as the last position in the array to display
+
+/*function handleImages() {
     getDogImages();
     displayImages();
 }
@@ -34,4 +56,6 @@ function handleForm() {
   });
 }
 
-$(handleForm);
+$(handleForm);*/
+
+$(watchForm);
